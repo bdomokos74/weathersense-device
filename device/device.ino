6ee4002bb27b4cdbb768c99a64d54eece4110eee
Balint Domokos <bdomokos74@gmail.com>
2020-10-31 20:38:39 +0100

    Fix sleeping

--- device/device.ino
+++ device/device.ino
@@ -23,12 +23,13 @@ const char* password = WIFI_PW;
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
 static const char* connectionString = HUZZAH32_DEV_CONN_STR;
-const char *messageData = "{\"messageId\":%d, \"Temperature\":%f}";
+const char *messageData = "{\"messageId\":%d, \"Temperature\":%f, \"devmsg\":%s}";
+const char *devData = "{\"bootCnt\":%d, \"wa_reason\":%d}";
 static bool hasIoTHub = false;
 static bool hasWifi = false;
 int messageCount = 1;
 static bool messageSending = true;
-unsigned long send_interval_ms = 0;
+unsigned long start_interval_ms = 0;
 
 // 1wire
 const int oneWireDev = 0;
@@ -54,6 +55,23 @@ int ledPin = 13;
 Adafruit_7segment sseg = Adafruit_7segment();
 static bool hasSevenSeg = false;
 int sevenSegAddr = 0x70;
+
+// deep sleep
+#define uS_TO_S_FACTOR 1000000
+#define SLEEP_TIME_SEC 300
+RTC_DATA_ATTR int bootCnt = 0;
+
+void log_wakeup(esp_sleep_wakeup_cause_t reason) {
+  
+  switch(reason) {
+  case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", reason); break;
+  }
+}
 
 static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
 {
@@ -135,10 +153,15 @@ void initWifi() {
   delay(10);
   WiFi.mode(WIFI_AP);
   WiFi.begin(ssid, password);
+  int retry = 10;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
-    hasWifi = false;
+    if(retry==0) {
+      hasWifi = false;
+      return;
+    }
+    --retry;
   }
   hasWifi = true;  
 }
@@ -166,73 +189,34 @@ void initSevenSeg() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("ESP32 Device Initializing...");
-  Serial.println("Starting connecting WiFi.");
-
-  initWifi();
-  initHub();
-  Wire.begin();
-  initSevenSeg();
-  
-  Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
-  Esp32MQTTClient_SetMessageCallback(MessageCallback);
-  Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
-  Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
-  Serial.println("Start sending events.");
-  randomSeed(analogRead(0));
-  send_interval_ms = millis();
-
-  sensors.begin();
-  //readOneWireAddresses();
-  if(sensors.getAddress(tempDeviceAddress, oneWireDev)){
-    readAddress(tempDeviceAddress);
-    foundOneWireDevice = true;
-    Serial.print("Device found ");
-    Serial.println(addr);
-  } else {
-    Serial.println("Device NOT FOUND");
-  }
-
-  pinMode(ledPin, OUTPUT);
-  send_interval_ms = 0;
-}
-
-void sendData() {
+void sendData(char* msg) {
 
     // Send teperature data
     char messagePayload[MESSAGE_MAX_LEN];
-   
-    snprintf(messagePayload, MESSAGE_MAX_LEN, messageData, messageCount++, tempC);
+    snprintf(messagePayload, MESSAGE_MAX_LEN, messageData, messageCount++, tempC, msg);
     Serial.println(messagePayload);
     EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
     Esp32MQTTClient_SendEventInstance(message);
 
 }
 
-void loop() {
+void run(char* msg) {
   
   if (hasWifi && hasIoTHub)
   {
-    if ( messageSending && (send_interval_ms==0 ||
-        ((millis() - send_interval_ms) >= INTERVAL)) )
+    if ( messageSending)
     {
-      Serial.println("In loop");
-      Serial.println(send_interval_ms);
       
       flashLed();
       
       readTemp(0);
       
-      sendData();
+      sendData(msg);
       
       if(hasSevenSeg) {
         sseg.println(tempC);
         sseg.writeDisplay();
       }
-
-      send_interval_ms = millis();
     }
     else
     {
@@ -241,5 +225,79 @@ void loop() {
   } else {
     flashLedErr();
   }
-  delay(10);
+  
+}
+
+void setup() {
+  start_interval_ms = millis();
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("ESP32 Device Initializing...");
+  
+  ++bootCnt;
+  Serial.println("Boot number: " + String(bootCnt));
+  esp_sleep_wakeup_cause_t reason;
+  reason = esp_sleep_get_wakeup_cause();
+  log_wakeup(reason);
+  if(bootCnt==1 || reason==ESP_SLEEP_WAKEUP_TIMER) 
+  {
+    Serial.println("Starting connecting WiFi.");
+    initWifi();
+    
+    if(hasWifi) {
+      Serial.print("Wifi done (ms): ");
+      Serial.println(millis()-start_interval_ms);
+      initHub();
+    
+      Serial.print("IoTHUB done (ms): ");
+      Serial.println(millis()-start_interval_ms);
+      Wire.begin();
+      initSevenSeg();
+      
+      Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+      Esp32MQTTClient_SetMessageCallback(MessageCallback);
+      Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
+      Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
+      Serial.println("Start sending events.");
+      randomSeed(analogRead(0));
+      
+      sensors.begin();
+      //readOneWireAddresses();
+      if(sensors.getAddress(tempDeviceAddress, oneWireDev)){
+        readAddress(tempDeviceAddress);
+        foundOneWireDevice = true;
+        Serial.print("Device found ");
+        Serial.println(addr);
+      } else {
+        Serial.println("Device NOT FOUND");
+      }
+    
+      pinMode(ledPin, OUTPUT);
+      
+      Serial.print("OneWire done (ms): ");
+      Serial.println(millis()-start_interval_ms);
+
+      char devMsg[MESSAGE_MAX_LEN];
+      snprintf(devMsg, MESSAGE_MAX_LEN, devData, bootCnt, (int)reason);
+      
+      run(devMsg);
+      
+    } else {
+      Serial.print("No wifi! (ms): ");
+      Serial.println(millis()-start_interval_ms);
+    }
+    
+    if(bootCnt==1 || reason==ESP_SLEEP_WAKEUP_TIMER) {
+      esp_sleep_enable_timer_wakeup(uS_TO_S_FACTOR * SLEEP_TIME_SEC);  
+    }
+  }
+  
+  Serial.print("Elapsed ms: ");
+  Serial.println((millis()-start_interval_ms));
+  Serial.println("Go sleep");
+  esp_deep_sleep_start();  
+}
+
+void loop() {
+  //delay(10);
 }
